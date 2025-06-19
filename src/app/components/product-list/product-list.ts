4157b628fad95678c2d2c4024976da0d1a277bf1
Pietro Vincenzi <pietro.vincenzi03@gmail.com>
2025-06-19 15:53:04 +0100

    +product list modified

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../model/product.model';
+import { CommonModule } from '@angular/common'; 
 
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css'
 })
-export class ProductList {
+export class ProductList implements OnInit {
     products: Product[] = [];
+    constructor(private productService: ProductService) {}
+
+      ngOnInit(): void {
+    this.productService.getAllProducts().subscribe({
+      next: (data) => this.products = data,
+      error: (err) => console.error('errore getAllProduct', err)
+    });
+  }
 }
