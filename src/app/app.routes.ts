3c80351f5c2cad8c9cfde26d898eb1b377aca2ef
Pietro Vincenzi <pietro.vincenzi03@gmail.com>
2025-06-19 16:41:40 +0100

    +collegamento a product detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProductList } from './components/product-list/product-list';
+import { ProductDetail } from './components/product-detail/product-detail';
 
 
 export const routes: Routes = [
   { path: '', component: ProductList },  
+  { path: 'product/:id', component: ProductDetail}
 ];
--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../model/product.model';
 import { CommonModule } from '@angular/common'; 
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule , RouterModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css'
 })
