5651d4688134fd89efffa7d489a2c3cc77c3b67b
Pietro Vincenzi <pietro.vincenzi03@gmail.com>
2025-08-13 00:46:43 +0100

    Pulizia codice

--- src/app/components/product-detail/product-detail.ts
+++ src/app/components/product-detail/product-detail.ts
@@ -42,9 +42,6 @@ export class ProductDetail implements OnInit {
         next: (data) => {
           this.product = data;
           this.maxQuantity = this.product?.quantity; 
-          if(this.maxQuantity == 0){
-            this.quantity=0;
-          }
           console.log(this.maxQuantity);
         },
         error: (err) => console.error('Errore nel prodotto passato:', err)
